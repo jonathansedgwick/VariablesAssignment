154bbb2224c3493ca619f454761e46feae153a32
JONATHAN SEDGWICK <jonathan.sedgwick25@cls-app-03.local>
2022-08-17 08:10:02 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var myByte: Byte = 5
+    var myShort: Short = 10
+    var myInt: Int = 250
+    var myLong: Long = 100000L
+    var myFloat: Float = 3.13F
+    var myDouble: Double = 4.3245
+    var myBoolean: Boolean = false
+    var myChar: Char = 'a'
+
+    println("This variable is a byte: $myByte")
+    println("This variable is a short: $myShort")
+    println("This variable is an int: $myInt")
+    println("This variable is a long: $myLong")
+    println("This variable is a float: $myFloat")
+    println("This variable is a double: $myDouble")
+    println("This variable is a boolean: $myBoolean")
+    println("This variable is a char: $myChar")
+}
